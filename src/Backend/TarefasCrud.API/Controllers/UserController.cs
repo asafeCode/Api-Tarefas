6abbcd4efe5d021d673346b2ff6e14573d5131f3
asafeCode <matt.asafe@gmail.com>
2025-12-08 18:28:12 -0300

    aa

--- src/Backend/TarefasCrud.API/Controllers/UserController.cs
+++ src/Backend/TarefasCrud.API/Controllers/UserController.cs
@@ -11,6 +11,16 @@ namespace TarefasCrud.API.Controllers;
 
 public class UserController : TarefasCrudControllerBase
 {
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+    public async Task<IActionResult> Register(
+        [FromServices] IRegisterUserUseCase useCase,
+        [FromBody] RequestRegisterUserJson request)
+    {
+        var result = await useCase.Execute(request);
+
+        return Created(string.Empty, result);
+    }
    
     [HttpGet]
     [AuthenticatedUser]
